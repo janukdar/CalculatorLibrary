3897d71a56f4624b6000fd2fc0de1fda5ed8c8b0
Dariusz Janukowicz <dariusz.janukowicz@orange.com>
2020-10-24 11:25:21 +0200

    Add and subtract functions

---
+++ calculator.py
@@ -0,0 +1,7 @@
+"""Calculator library containing basic math operations - RealPython excecise"""
+
+def add(item_1, item_2):
+    return item_1 + item_2
+
+def subtract(item_1, item_2)
+    return item_1 - item_2
