8ab016989a016d3b99d519b9983b6f36ac8961d2
Dariusz Janukowicz <dariusz.janukowicz@orange.com>
2020-10-24 13:26:40 +0200

    Added unit tests for the calculator

--- calculator.py
+++ calculator.py
@@ -1,7 +1,9 @@
 """Calculator library containing basic math operations - RealPython excecise"""
 
+
 def add(item_1, item_2):
     return item_1 + item_2
 
-def subtract(item_1, item_2)
+
+def subtract(item_1, item_2):
     return item_1 - item_2
