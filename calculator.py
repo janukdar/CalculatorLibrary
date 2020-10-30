c4bb4d53915743c6f9bbfdeed8cb22b83801eac5
Dariusz Janukowicz <dariusz.janukowicz@orange.com>
2020-10-30 09:51:17 +0100

    Corrected tabs

--- calculator.py
+++ calculator.py
@@ -2,13 +2,13 @@
 
 
 def add(item_1, item_2):
-	print("Adding two numbers ...")
-	return item_1 + item_2
+    print("Adding two numbers ...")
+    return item_1 + item_2
 
 
 def subtract(item_1, item_2):
-	return item_1 - item_2
+    return item_1 - item_2
 
 
 def multiply(item_1, item_2):
-	return item_1 * item_2
+    return item_1 * item_2
