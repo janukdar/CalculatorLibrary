77706f16f057e3532fff32d9b3727841e335cb0f
Dariusz Janukowicz <dariusz.janukowicz@orange.com>
2020-10-30 09:48:40 +0100

    Corrected spaces

--- calculator.py
+++ calculator.py
@@ -3,12 +3,12 @@
 
 def add(item_1, item_2):
 	print("Adding two numbers ...")
-    return item_1 + item_2
+	return item_1 + item_2
 
 
 def subtract(item_1, item_2):
-    return item_1 - item_2
+	return item_1 - item_2
 
 
 def multiply(item_1, item_2):
-    return item_1 * item_2
+	return item_1 * item_2
